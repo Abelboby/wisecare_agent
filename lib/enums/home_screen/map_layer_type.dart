5bff9794359a7e32bd73e024d3b47585d13185a2
abelboby <abeldavisboby@gmail.com>
2026-03-07 21:42:09 +0530

    Add: Introduce MapLayerType enum for map base layer styles

---
+++ lib/enums/home_screen/map_layer_type.dart
@@ -0,0 +1,10 @@
+part of 'package:wisecare_agent/enums/app_enums.dart';
+
+/// Map base layer style for the map tab. Used for layer picker bottom sheet.
+enum MapLayerType {
+  standard,
+  dark,
+  light,
+  voyager,
+  terrain,
+}
